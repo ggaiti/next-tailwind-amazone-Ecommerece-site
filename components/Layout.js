5212e72cee0940a5773395e4524e7b2ab820c50b
ggaiti <ggaiti@hotmail.com>
2023-03-30 21:58:03 -0400

    Styled Layout of Home page

---
+++ components/Layout.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + " - Amazone" : "Amazone"}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/" className="text-lg font-bold">
+              Amazone
+            </Link>
+            <div>
+              <Link href="/cart" className="p-2">
+                Cart
+              </Link>
+              <Link href="/Login" className="p-2">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright © 2022 Amazone</p>
+        </footer>
+      </div>
+    </>
+  );
+}
